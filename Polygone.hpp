d9ea00487f31a760f9cfaf23af2f3ce1676be110
LouisMichel99 <82646460+LouisMichel99@users.noreply.github.com>
2022-01-14 07:49:33 +0000

    Poly initiated

--- Polygone.hpp
+++ Polygone.hpp
@@ -1,2 +1,23 @@
 #include "Point2D.hpp"
 
+/*
+- sommets : Vector<Point2D<T>>
++ Polygone( )
++ Polygone(Vector<Point2D<T>> listeSommets)
++ Polygone(Polygone<T, S> poly)
++ getSommets( ) : Vector<Point2D<T>>
++ setSommets(Vector<Point2D<T>> listeSommets) : void + addPoint(Point2D<T>) : void
++ translate(T x, T y) : void
+*/
+
+template <typename T>
+class Polygone {
+    protected:
+        
+
+    public:
+        Polygone();
+        Polygone(vector <Point2D<T>> listeSommets);
+        Polygone (Polygone <T, D> poly);
+
+};
